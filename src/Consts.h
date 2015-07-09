063a405a2f159180c340b6ac82ce6dbdf83bebd8
Maya <student@ubuntu.(none)>
2015-07-09 12:45:29 -0400

    Type definitions

---
+++ src/Consts.h
@@ -0,0 +1,17 @@
+/*
+ * Consts.h
+ *
+ *  Created on: Jul 9, 2015
+ *      Author: student
+ */
+
+#ifndef CONSTS_H_
+#define CONSTS_H_
+
+/**
+ * This two variables define the boolean values
+ */
+#define FALSE 0
+#define TRUE 1
+
+#endif /* CONSTS_H_ */
