d66a31b0dd2f2cbd7ac33e6e549338f64013f976
giladmaya <ms.maya.gilad@gmail.com>
2015-07-17 19:50:15 -0400

    Implemented .data numbers extractions Fixed bug in .string extraction - was missing 0---0 data after a string

--- src/FirstTransition.h
+++ src/FirstTransition.h
@@ -18,6 +18,7 @@ void process_line(line_info* info, unsigned int* ic, unsigned int* dc);
 
 void process_data(line_info* info, unsigned int* dc, char* word, char* type);
 void process_string(line_info* info, unsigned int* dc);
+void process_numbers(line_info* info, unsigned int* dc);
 
 void process_operation(line_info* info,unsigned int* ic, char* word);
 void process_extern(line_info* info);
