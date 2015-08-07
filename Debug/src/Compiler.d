4148ef7b1809e5428df9818125858cae43f69c33
maiki89g <maiki89g@gmail.com>
2015-08-07 11:42:09 -0400

    Simplify operation bits code

--- Debug/src/Compiler.d
+++ Debug/src/Compiler.d
@@ -1,6 +1,6 @@
 src/Compiler.d: ../src/Compiler.c ../src/FirstTransition.h ../src/Types.h \
- ../src/Enums.h ../src/SecondTransition.h ../src/Utilities.h \
- ../src/Consts.h
+ ../src/Enums.h ../src/Consts.h ../src/SecondTransition.h \
+ ../src/Utilities.h
 
 ../src/FirstTransition.h:
 
@@ -8,8 +8,8 @@ src/Compiler.d: ../src/Compiler.c ../src/FirstTransition.h ../src/Types.h \
 
 ../src/Enums.h:
 
+../src/Consts.h:
+
 ../src/SecondTransition.h:
 
 ../src/Utilities.h:
-
-../src/Consts.h:
